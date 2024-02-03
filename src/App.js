65e319a41763373938603bde1bd42e5c6045db65
ChanLee_KR <cl700120@gmail.com>
2024-02-03 22:19:45 +0900

    default settings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import ProductListPage from "./pages/ProductListPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import PaymentPage from "./pages/PaymentPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<ProductListPage />}></Route>
+            <Route path="/login" element={<LoginPage />}></Route>
+            <Route path="/register" element={<RegisterPage />}></Route>
+            <Route path="/detail" element={<ProductDetailPage />}></Route>
+            <Route path="/payment" element={<PaymentPage />}></Route>
+        </Routes>
+    );
 }
-
-export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import headerLogo from '../assets/header-logo.svg'
+
+export default function Header() {
+    return (
+        <div>
+            <img src={headerLogo} alt='Shop PBL' />
+        </div>
+    )
+}
