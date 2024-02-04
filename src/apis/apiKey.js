f1b0159ef1b11410ba11abc1af93940c306616f0
ChanLee_KR <cl700120@gmail.com>
2024-02-04 12:54:17 +0900

    modify api key

--- src/apis/apiKey.js
+++ src/apis/apiKey.js
@@ -1,10 +1,10 @@
 const config = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyDSif7tVo8pk6MHE8WHK1xf5sVOiLtJxAc",
+    authDomain: "shop-pbl.firebaseapp.com",
+    projectId: "shop-pbl",
+    storageBucket: "shop-pbl.appspot.com",
+    messagingSenderId: "965958164335",
+    appId: "1:965958164335:web:c4972c76cfd3684ccd529a"
 }
 
 export default config;
